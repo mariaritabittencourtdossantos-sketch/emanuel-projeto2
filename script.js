cb5d7aaf8800522515fed93900b9f4f4b00602c7
mariaritabittencourtdossantos-sketch <maria.ritabittencourtdos.santos@escola.pr.gov.br>
2026-07-08 08:48:00 -0300

    l

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const botaoModo = document.getElementById('toggle-mode');
 
 botaoModo.addEventListener('click', () => {
-    // Alterna a classe do modo claro no body
     document.body.classList.toggle('light-mode');
     
-    // Verifica qual modo está ativo para atualizar o texto do botão
     if (document.body.classList.contains('light-mode')) {
         botaoModo.textContent = 'Modo Escuro';
     } else {
